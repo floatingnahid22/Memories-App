c5d59e739492bae1faf289f958bb36002fbb8547
floatingnahid22 <nahid.uap.github@gmail.com>
2022-08-31 23:15:18 +0600

    finish

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+
+import postRoutes from "./routes/posts.js";
+import userRouter from "./routes/user.js";
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+app.use("/user", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("App is Running");
+});
+
+const CONNECTION_URL =
+  "mongodb+srv://nahid22:318zXVnGPiuTAqJt@cluster0.u92abk9.mongodb.net/?retryWrites=true&w=majority@cluster0.u92abk9.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
